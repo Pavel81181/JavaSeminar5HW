f90fe098c6c342f1f8414b791c02dc267dabd1d6
Бажин Павел Аркадьевич <p.bazhin@agro.local>
2023-08-13 15:14:03 +0300

    Сделал ДЗ

---
+++ src/main/java/org/example/Task1/Task1.java
@@ -0,0 +1,44 @@
+package org.example.Task1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//1) Подсчитать количество вхождения каждого слова
+//        Пример:
+//        Россия идет вперед всей планеты. Планета — это не Россия.
+//        toLoverCase().
+public class Task1 {
+    public static void main(String[] args) {
+        String str = "Россия идет вперед всей планеты. Планета — это не Россия.";
+        printWords(getCountWords(str));
+    }
+
+    /**
+     * @apiNote Вывод слов в консоль
+     * @param map
+     */
+    private static void printWords(Map<String, Integer> map) {
+        System.out.println("Количество вхождений слов в тексте:");
+        for (String key: map.keySet()) {
+            System.out.printf("%s - %d\n", key, map.get(key));
+        }
+    }
+
+    /**
+     * @apiNote Подсчитать количество вхождения каждого слова
+     * @param str Текст, в котором считаются слова
+     * @return Слова, с количеством вхождений
+     */
+    private static Map<String, Integer> getCountWords(String str) {
+        Map<String, Integer> map = new HashMap<>();
+        str = str.toLowerCase().replaceAll("[.—:;,!?]", "");
+        String[] words = str.split("\\s+");
+
+        for (String item: words) {
+            map.putIfAbsent(item, 0);
+            map.put(item, map.get(item)+1);
+        }
+        return map;
+    }
+}
+
